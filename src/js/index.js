98ae4c8ff8d700dabc693ed51a9e103efa437685
kingkazuma7 <nasionasio7@gmail.com>
2020-05-15 10:12:43 +0900

    [Update] ハンバーガーメニュー追加

--- src/js/index.js
+++ src/js/index.js
@@ -11,4 +11,35 @@ $(function () {
       }
     });
   });
+
+  /*
+  hum
+  */
+  var $nav = $("#navArea");
+  var $btn = $(".toggle_btn");
+  var $mask = $("#mask");
+  var open = "open"; // class
+  // menu open close
+  $btn.on("click", function () {
+    if (!$nav.hasClass(open)) {
+      $nav.addClass(open);
+    } else {
+      $nav.removeClass(open);
+    }
+  });
+  // mask close
+  $mask.on("click", function () {
+    $nav.removeClass(open);
+  });
+});
+
+$(function () {
+  $('a[href^="#"]').click(function () {
+    var speed = 500;
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? "html" : href);
+    var position = target.offset().top;
+    $("html, body").animate({ scrollTop: position }, speed, "swing");
+    return false;
+  });
 });
